18ec8c4988cf6b674b26fee28365d592d2a45a4e
Shubham Bajaj <shubham2bajaj@gmail.com>
2023-05-01 22:25:13 +0200

    added DB connect code

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const keys = require('./keys')
+
+const app = express();
+
+var cors = require("cors");
+app.use(cors());
+
+mongoose
+  .connect(keys.__MongoTOKEN__) // create Constants.js file having token value
+  .then(() => console.log("connected to DB"))
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
